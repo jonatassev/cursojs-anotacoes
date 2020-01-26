0d59b50a8717cc9ce822daa437bb9e37ee78b655
Jonatas Severino <jojo80ieq@gmail.com>
2020-01-25 23:30:53 -0200

    destructing

---
+++ fundamentos/destructing1.js
@@ -0,0 +1,37 @@
+// Novo recurso ES2015
+// Utilizado para retirar atributos de um objeto
+
+const pessoa = {
+    nome: 'Ana',
+    sobrenome: 'Maria',
+    idade: 15,
+    endereco: {
+        cidade: 'Tubarão',
+        estado: "SC",
+        pais: "Brasil"
+    }
+}
+// retirando o atributo de forma convencional:
+
+let nomeDaPessoa = pessoa.nome
+console.log(nomeDaPessoa)
+
+// com destructing:
+
+const {nome, sobrenome} = pessoa
+
+console.log(nome, sobrenome)
+
+// desta forma, com uma linha, retiramos atributos da variável e atribuimos as suas respectivas variáveis
+
+// Da forma a seguir, alteramos os nomes das variáveis
+
+const {nome: n, sobrenome: s} = pessoa
+console.log(n,s)
+
+// quando definimos um atributo que não existe, ele retorna undefined
+// para evitarmos isso, atribuímos um valor padrão, assim se não tiver nada definido ele retorna o valor padrão
+const {sexo, altura = 0.0 } = pessoa 
+
+// Objeto dentro de objeto
+const { endereco: {cidade, estado}} = pessoa
---
+++ fundamentos/destructing2.js
@@ -0,0 +1,10 @@
+// destructing em Array
+const [a] = [10]
+console.log(a)
+
+const [n1, , n3, , n5, n6 = 0] = [10, 7, 9, 8]
+console.log(n1, n3, n5, n6)
+
+const [, [, nota]] = [[, 8, 8], [9, 6, 8]]
+console.log(nota)
+// não é muito recomendado dessa forma, pois fica mais complexo para o entedimento
